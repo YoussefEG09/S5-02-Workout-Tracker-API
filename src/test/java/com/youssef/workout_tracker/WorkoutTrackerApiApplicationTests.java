97a0b174cb3196652f51eddec67db1845fe14522
YoussefEG09 <youssef.eg09@gmail.com>
2026-02-17 11:56:44 +0100

    feat: adding files to GitHub repository

---
+++ src/test/java/com/youssef/workout_tracker/WorkoutTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youssef.workout_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
